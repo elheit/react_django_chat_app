d91350226a630b20f4dce6563049a0d97181fb73
elheit <meslamineelheit@gmail.com>
2024-06-28 18:12:05 +0100

    [ADD] the views and urls and models for the server app and also the sereliazer that gonna transfer our data to json so it can send throw api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External App
     'drf_spectacular',
+    'rest_framework',
     # Internal App
     'account',
     'server',
@@ -125,6 +126,9 @@ AUTH_USER_MODEL = "account.Account"
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
--- backend/server/views.py
+++ backend/server/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import ServerSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from django.db.models import Count
+from .models import Server
 
-# Create your views here.
+class ServerListViewSet(viewsets.ViewSet):
+    queryset = Server.objects.all()
+    
+    def list(self, request):
+        category = request.query_params.get('category')
+        by_user = request.query_params.get('by_user') == 'true'
+        qty = request.query_params.get('qty')
+        server_id = request.query_params.get('server_id')
+        with_num_members = request.query_params.get('with_num_members') == 'true'
+
+        if by_user or server_id and not request.user.is_authenticated:
+            raise AuthenticationFailed()
+        
+        if category:
+            self.queryset = self.queryset.filter(category__name=category)
+
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(members=user_id)
+        
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count('members') )
+
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
+
+        if server_id:
+            try:
+                self.queryset = self.queryset.filter(id=server_id)
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"Server with id {server_id} Not Fuound")
+            except ValueError:
+                raise ValueError(detail=f"An Error is Accure")
+        
+
+        serializer = ServerSerializer(self.queryset, many=True, context={'num_members': with_num_members})
+        return Response(serializer.data)
