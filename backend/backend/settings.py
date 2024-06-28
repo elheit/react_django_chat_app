413f5bc79c24d9a359569017ef98b25db78036d0
elheit <meslamineelheit@gmail.com>
2024-06-28 14:33:51 +0100

    [ADD] the api schemma and ui using drf_spectacular

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,6 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External App
+    'drf_spectacular',
+    # Internal App
     'account',
     'server',
 ]
@@ -118,3 +121,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = "account.Account"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+}
