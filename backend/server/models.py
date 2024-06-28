144ac62a73debdd1ab89bd1bdf1453501bd731ea
elheit <meslamineelheit@gmail.com>
2024-06-29 00:00:26 +0100

    [ADD] adding validation function for the images and icons

--- backend/server/models.py
+++ backend/server/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
+from .validators import validate_icon_image_size, validate_image_file_exstension
 
 # Create your models here.
 def server_icon_upload_path(instance, filename):
@@ -16,7 +17,12 @@ def category_icon_upload_path(instance, filename):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
+    icon = models.FileField(
+        upload_to=category_icon_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_icon_image_size, validate_image_file_exstension]
+    )
 
     def save(self, *args, **kwargs):
         if self.id: # that mean the data already exist not creating new one
@@ -53,8 +59,18 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='channel_server')
-    banner = models.ImageField(upload_to=server_banner_upload_path, null=True, blank=True)
-    icons = models.ImageField(upload_to=server_icon_upload_path, null=True, blank=True)
+    banner = models.ImageField(
+        upload_to=server_banner_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_image_file_exstension]
+    )
+    icons = models.ImageField(
+        upload_to=server_icon_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_icon_image_size, validate_image_file_exstension]
+    )
 
     def save(self, *args, **kwargs):
         
