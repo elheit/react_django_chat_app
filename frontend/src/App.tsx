04b0e82fd5e6fcc901d5b89afede993459ce11eb
elheit <meslamineelheit@gmail.com>
2024-06-29 11:07:23 +0100

    [ADD] the basic definition for the frontend part of the project using react and typescript and also react-router-dom

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />}/>
+    </Route>
+  )
+)
+
+const App = () => {
+  return(
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
