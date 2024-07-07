8a082d9762c6dd7028e81c2eb2756e9c3ead553e
elheit <meslamineelheit@gmail.com>
2024-07-07 13:50:10 +0100

    [REF] using cookies for the dark mode instead of localStorage

--- frontend/src/components/ToggleColorMode.tsx
+++ frontend/src/components/ToggleColorMode.tsx
@@ -1,29 +1,34 @@
 import { CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import React from "react";
 import createMuiTheme from "../theme/theme";
 import { ColorModeContext } from "../context/DarkModeContext";
+import Cookies from "js-cookie";
 
 interface ToggleColorModeProps {
   children: React.ReactNode;
 }
 
 const ToggleColorMode: React.FC<ToggleColorModeProps> = ({ children }) => {
-  const [mode, setMode] =
-    useState<"light" | "dark">(
-      () => localStorage.getItem("colorMode") as "light" | "dark"
-    ) || (useMediaQuery("([prefers-color-scheme: dark])") ? "dark" : "light");
+  const storedMode = Cookies.get("colorMode") as "light" | "dark";
+  const preferedDarkMode = useMediaQuery("([prefers-color-scheme: dark])");
+  const defaultMode = storedMode || (preferedDarkMode ? "dark" : "light");
 
-  const toggleColorMode = useCallback(() => {
+  const [mode, setMode] = useState<"light" | "dark">(defaultMode);
+
+  const toggleColorMode = React.useCallback(() => {
     setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("colorMode", mode);
+    Cookies.set("colorMode", mode);
   }, [mode]);
 
   const colorMode = useMemo(() => ({ toggleColorMode }), [toggleColorMode]);
 
-  const theme = useMemo(() => createMuiTheme(mode || "light"), [mode]);
+  const theme = React.useMemo(() => createMuiTheme(mode), [mode]);
+
+  console.log("Retrieved mode:", mode);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
